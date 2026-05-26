a86dbee747366bef526ba45ef0539baa6b6b8c69
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-05-26 14:55:08 -0300

    feat: Initialize student data in StudentController using @PostConstruct

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/controllers/StudentController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/example/demo/controllers/StudentController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controllers;
 
 import com.example.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +14,18 @@ import java.util.List;
 public class StudentController {
     // define endpoint for "/students" - return a list of students
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students = new ArrayList<>();
 
+    @PostConstruct
+    public void loadData() {
         students.add(new Student("Rodrigo", "Alexandre"));
         students.add(new Student("Josely", "Vieira"));
         students.add(new Student("Mary", "Brown"));
         students.add(new Student("Alex", "Blue"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
     }
 
